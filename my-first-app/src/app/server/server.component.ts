15bd3c4606b58677e9ff2c65c27831d9bda3b1c1
MustafaSidiqi <mustafasidiqi95@gmail.com>
2018-05-28 11:08:31 +0200

    Add files via upload

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,25 @@
+import { Component} from "@angular/core";
+
+@Component({
+  selector: "app-server",
+  templateUrl: "./server.component.html",
+  styles: [
+
+  ]
+})
+export class ServerComponent {
+  serverId: number = 404;
+  serverStatus: string = "Offline";
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "Online" : "Offline";
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === "Online" ? "green" : "red";
+  }
+}
---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = "TestServer";
+  serverCreated = false;
+  servers = ['testserver', 'testserver 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created, and name is ";
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+}
